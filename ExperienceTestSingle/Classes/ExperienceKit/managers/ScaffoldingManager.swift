69cfe19e7a2cc7b3d6ea3b9be284306943f67be2
katherinepamina <katherinelin2016@u.northwestern.edu>
2016-05-23 10:44:22 -0500

    audio backgrounding

---
+++ ExperienceTestSingle/Classes/ExperienceKit/managers/ScaffoldingManager.swift
@@ -0,0 +1,68 @@
+//
+//  ScaffoldingManager.swift
+//  ExperienceTestSingle
+//
+//  Created by Hyung-Soon on 5/12/16.
+//  Copyright © 2016 Hyung-Soon. All rights reserved.
+//
+
+import Foundation
+import Parse
+
+class ScaffoldingManager: NSObject {
+
+    var curPulledObject: PFObject?
+    var _experienceManager: ExperienceManager
+    var insertableMomentBlocks: [MomentBlockSimple] = []
+    init(experienceManager: ExperienceManager, insertableMomentBlocks: [MomentBlockSimple]=[] ) {
+        self._experienceManager = experienceManager
+        self.insertableMomentBlocks = insertableMomentBlocks
+        super.init()
+    }
+    
+    func getPossibleInsertion(withInformation: Any?) -> MomentBlockSimple? {
+     
+        //parse out user defined filters
+        var label: String?
+        if let infoDict = withInformation as? [String : String] {
+            label = infoDict["label"]
+        }
+        
+        var curGeoPoint = PFGeoPoint(location: _experienceManager.dataManager!.currentLocation!)
+        var query = PFQuery(className: "WorldObject")
+        var geoQuery = query.whereKey("location", nearGeoPoint: curGeoPoint, withinKilometers: 0.1)
+        var object: PFObject?
+        if (label != nil) {
+            object = geoQuery.whereKey("label", equalTo: label!).getFirstObject()
+        }
+        else {
+            //object = geoQuery.getFirstObject()
+            return nil
+        }
+        
+        print("geoquery result: \(geoQuery)")
+        print("query result: \(object)")
+        curPulledObject = object
+        
+        let bestMomentBlock = getBestMomentBlock(label!)
+        if bestMomentBlock != nil {
+            print("--possible insertion:\(bestMomentBlock!.title)--")
+            return bestMomentBlock
+        }
+        return nil
+    }
+    
+    //TODO) ALSO NEED TO RANK THE OBJECTS THAT WERE PULLED 
+    //(IN CASE THERE ARE MULTIPLE THAT MATCH THE CONDITIONS)
+    
+    //rank the possible insertable MomentBlocks
+    func getBestMomentBlock(label:String) -> MomentBlockSimple? {
+        for momentBlock in insertableMomentBlocks {
+            if ( momentBlock.requirement?.objectLabel == label ) {
+                return momentBlock
+            }
+        }
+        return nil
+    }
+    
+}
---
+++ ExperienceTestSingle/Classes/ExperienceKit/moments/Moment.swift
@@ -0,0 +1,79 @@
+//
+//  Moment.swift
+//  Zombies Interactive
+//
+//  Created by Henry Spindell on 10/2/15.
+//  Copyright © 2015 Scott Cambo. All rights reserved.
+//
+
+import Foundation
+
+/// A component that makes up part of a MomentBlock; from an MomentBlock_Simple with the user to collect data,
+/// to simple audio to advance the experience.
+/// DO NOT instantiate this class, instead, use one of its subclasses.
+class Moment: NSObject {
+    
+    /*
+        How do we prevent this from being initialized?
+    */
+    
+    /// The title of the moment; its role or purpose within the experience
+    var title: String
+    
+    /// Whether or not this moment may be interrupted to dynamically insert 
+    /// an MomentBlock_Simple from the OpportunityManager. Always defaults to false.
+    var isInterruptable: Bool
+    
+    var canEvaluateOpportunity: Bool
+    
+    var duration: Float = 0
+    var momentStarted = false
+    var isPaused = true
+    let eventManager = EventManager()
+    override var description:String {
+        return self.title
+    }
+    
+    init(title: String, isInterruptable:Bool=false, canEvaluateOpportunity:Bool=false){
+        // WARNING: DO NOT INIT A Moment(), USE ONLY SUBCLASSES - need to find solution
+        // check the caller
+        self.title = title
+        self.isInterruptable = isInterruptable
+        self.canEvaluateOpportunity = canEvaluateOpportunity
+    }
+    
+    func start(){
+        print("  \n(Moment::start)    Starting moment: \(self.title)")
+        momentStarted = true
+        // passed as string so it will conform to AnyObject
+        eventManager.trigger("startingMoment", information: ["duration": "\(duration)"])
+        self.play()
+    }
+
+    func play(){
+        self.isPaused = false
+    }
+
+    func pause(){
+        self.isPaused = true
+    }
+    
+    func finished(){
+        print("  (Moment::finished) Finished moment: \(self.title)")
+        
+        self.eventManager.trigger("nextMoment")
+    }
+    
+    /// Sets the moment to a state where it can be reused again to save memory
+    // could be changed to prepare()
+    // currently can't be used because the event listener sets multiple triggers
+    // for the same object and it screws up everything
+//    func reset() {
+//    
+//    }
+}
+
+
+
+
+
